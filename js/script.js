f2a03e2b23c59e5cf7e6ff94ed392d0edf4e0f8b
Olga <antoncikovaolga@gmail.com>
2021-12-15 12:29:34 +0300

    my second task (if/for) Udemy

--- js/script.js
+++ js/script.js
@@ -10,12 +10,38 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
+if (personalMovieDB.count <= 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    alert('Вы - киноман');
+} else {
+    alert('Произошла ошибка');
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+    console.log(personalMovieDB);
+}
+
+// let i = 0;
+// while (i < 2) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
+//     i++;
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//     } else {
+//         i--;
+//     }
+// }
+
---
+++ js/script_trainee.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// Условия 
+
+// if (4 === 4) {
+//     console.log('OK');
+// }
+
+// const num = 60;
+
+// if (num < 49){
+//     console.log('Error');
+// } else if (num >100){
+//     console.log('Слишком много');
+// } else {
+//     console.log('OK');
+// }
+
+// (num === 50) ? console.log('Ok') : console.log('Error');
+
+// switch (num) {
+//     case 49:
+//         console.log('Error');
+//         break;
+//     case 100:
+//         console.log('Too much');
+//         break;
+//     case 50:
+//         console.log('Bingo');
+//         break;
+//     default:
+//         console.log('Not now');
+//         break;
+// }
+
+// Циклы
+
+// let num = 50;
+
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
+    console.log(i);
+}
