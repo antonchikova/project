292cb0eb5744042014e86ff7234f46cf0e083f65
Olga <antoncikovaolga@gmail.com>
2021-12-17 10:47:19 +0300

    my third task(function) Udemy)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const numberOfFilms = + prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,38 +20,46 @@ const personalMovieDB = {
     privat: false
 };
 
-if (personalMovieDB.count <= 10) {
-    alert('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-    alert('Вы классический зритель');
-} else if (personalMovieDB.count > 30) {
-    alert('Вы - киноман');
-} else {
-    alert('Произошла ошибка');
-}
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', '');
 
-
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
-        personalMovieDB.movies[a] = b;
-    } else {
-        i--;
+        if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--;
+        }
     }
-    console.log(personalMovieDB);
 }
 
-// let i = 0;
-// while (i < 2) {
-//     const a = prompt('Один из последних просмотренных фильмов?', ''),
-//         b = prompt('На сколько оцените его?', '');
-//     i++;
-//     if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
-//         personalMovieDB.movies[a] = b;
-//     } else {
-//         i--;
-//     }
-// }
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count <= 10) {
+        alert('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+        alert('Вы классический зритель');
+    } else if (personalMovieDB.count > 30) {
+        alert('Вы - киноман');
+    } else {
+        alert('Произошла ошибка');
+    }
+}
+
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        const yourGenre = prompt(`Какой ваш любмый жанр  под номером ${i}`);
+        personalMovieDB.genres[i - 1]= yourGenre;
+    }
+}
+writeYourGenres();
 
--- js/script_trainee.js
+++ js/script_trainee.js
@@ -43,10 +43,68 @@
 // }
 
 
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        // break;
-        continue;
-    }
-    console.log(i);
-}
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// ФУНКЦИИ
+// let num = 9;
+// function showFirstMessage(text) {
+//     console.log(text);
+//     num = 4;
+// }
+// showFirstMessage('text');
+
+// console.log(num);
+
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+
+// console.log(calc(4,6));
+
+// let val1 = 2;
+//  function multiplyThis(n) {
+//         let ret = n * val1;
+//   return ret;
+//  }
+//  let multiplied = multiplyThis(6);
+
+//  console.log('example of scope:', multiplied);
+
+// function createCounter() {
+//     let counter = 0;
+//     const myFunction = function () {
+//         counter = counter + 1;
+//         return counter;
+//     };
+//     return myFunction;
+// }
+// const increment = createCounter();
+// const c1 = increment();
+// const c2 = increment();
+// const c3 = increment();
+// console.log('example increment', c1, c2, c3);
+
+
+// МЕТОДЫ И СВОЙСТВА 
+
+const str = 'test method';
+
+console.log(str.indexOf('e'));
+
+
+const nameOfPerson = 'Olga Antonchikova';
+console.log(nameOfPerson.slice(5, 10));
+console.log(nameOfPerson.substr(5, 5));
+
+const num = 12.5;
+console.log(Math.round(num));
+
+
+
